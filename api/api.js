8b4be1d476699bdd2519c178f66a3ed32bdb14fe
Eric Carboni <ericcarboni@gmail.com>
2017-02-20 16:01:16 -0500

    Add test call

--- api/api.js
+++ api/api.js
@@ -2,3 +2,10 @@ var http = require('http');
 var express = require('express');
 
 var app = express();
+
+app.get('/test', function(req, res) {
+    res.status(200).send('Successful');
+});
+
+app.listen(3000);
+
