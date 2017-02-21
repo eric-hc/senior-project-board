4a9b4d4cb7ac7a9e4d7d451a0bb2c0eeef844e71
Eric Carboni <ericcarboni@gmail.com>
2017-02-20 19:28:28 -0500

    Add packages

--- api/api.js
+++ api/api.js
@@ -1,11 +1,31 @@
 var http = require('http');
 var express = require('express');
-
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'This is our API' });
+});
+
 
 app.get('/test', function(req, res) {
     res.status(200).send('Successful');
 });
 
-app.listen(3000);
+app.get('/ships', function(req, res) {
+    res.status(200).send('Ships');
+});
+
+app.post('/hit', function(req, res) {
+
+});
+
+app.use('/api', router);
+app.listen(port);
 
