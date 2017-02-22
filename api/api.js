aef14ba1d9243dbe174c584bd0a15e63affb8ae7
Eric Carboni <ericcarboni@gmail.com>
2017-02-22 12:55:42 -0500

    Beautify

--- api/api.js
+++ api/api.js
@@ -3,30 +3,33 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.json({ message: 'This is our API' });
+router.get('/', function (req, res) {
+    res.json({
+        message: 'This is our API'
+    });
 });
 
 
-app.get('/test', function(req, res) {
+app.get('/test', function (req, res) {
     res.status(200).send('Successful');
 });
 
-app.get('/ships', function(req, res) {
+app.get('/ships', function (req, res) {
     res.status(200).send('Ships');
 });
 
-app.post('/hit', function(req, res) {
+app.post('/hit', function (req, res) {
 
 });
 
 app.use('/api', router);
 app.listen(port);
 console.log('Magic happening on port ' + port);
-
