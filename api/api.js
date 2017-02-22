ac130725c63d359505d648d00024d2045a9f0b76
Eric <ericcarboni@gmail.com>
2017-02-22 18:16:25 +0000

    Added routes

--- api/api.js
+++ api/api.js
@@ -11,22 +11,32 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    console.log('Request sent to API...');
+    next(); 
+});
+
 router.get('/', function (req, res) {
     res.json({
         message: 'This is our API'
     });
 });
 
-
-app.get('/test', function (req, res) {
-    res.status(200).send('Successful');
-});
-
-app.get('/ships', function (req, res) {
-    res.status(200).send('Ships');
-});
-
 app.post('/hit', function (req, res) {
+
+});
+
+router.route('/ships')
+    .get(function(req, res) {
+    res.json({
+        message: 'These are your ships, motherfucker'
+    });
+    })
+;
+
+router.route('/hit/:xy')
+    .post(function(req, res) {
 
 });
 
