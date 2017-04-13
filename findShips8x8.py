e68fcdbc8da03a65e99fa82bcb872268687a4b17
Eric <ericcarboni@gmail.com>
2017-04-13 21:26:56 +0000

    murder

--- findShips8x8.py
+++ findShips8x8.py
@@ -4,7 +4,7 @@ import math
 
 bus = smbus.SMBus(1) # Rev 2 Pi uses 1, Rev 1 uses 0
 # Scans 64 inputs on 4 MCP23017 port exapanders and returns changes
-mbrd = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]   # mbrd is the 8 columns of the battleship board this sets them to 11111111 : open w
+mdrd = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]   # mbrd is the 8 columns of the battleship board this sets them to 11111111 : open w
 DEVICE = [0x21, 0x22, 0x23, 0x24]  # 4 I2c Device address of the MCP23017s (A0-A2)
 GPIOn = [0x12, 0x13]
 IODIRA = 0x00 # APin direction register for first 8 ie 1 = input or 2= output
@@ -14,7 +14,7 @@ GPIOB  = 0x13 # B Register for inputs
 GPPUA = 0x0C  # Register for Pull ups A
 GPPUB = 0x0D  # Register for Pull ups B
 
-for i in range(4):
+for i in range(1):
   # for each of the 4 devices
   # Set all A 8 GPA pins as  input. ie set them to 1 oXFF = 11111111
   bus.write_byte_data(DEVICE[i], IODIRA, 0xFF)
@@ -27,7 +27,7 @@ for i in range(4):
 
 while True:
   # read the 8 registers
-  for k in range(4):
+  for k in range(1):
     for l in range(2):
       a = bus.read_byte_data(DEVICE[k], GPIOn[l])
       if a != mdrd[k + l]: # there has been a change
