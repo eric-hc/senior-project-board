d7b3dc99ff1dd16b4f4dfb6fecbeccc8eeb30c90
Eric Carboni <ericcarboni@gmail.com>
2017-04-26 21:43:03 +0000

    Update game over thing

--- api/game_over.py
+++ api/game_over.py
@@ -9,10 +9,11 @@ bus = smbus.SMBus(I2C_bus_number)
 # bus = smbus.SMBus(1) # Rev 2 Pi uses 1
 # this program scans 64 inputs on 4 MCP23017 port exapanders and returns changes
 
-# Create display instance on default I2C address (0x70) and bus number.
-display = Matrix8x8.Matrix8x8()
 # LED on MUX channel 2 = 1**2 channel 1
 bus.write_byte(I2C_address,2**1)
+
+# Create display instance on default I2C address (0x70) and bus number.
+display = Matrix8x8.Matrix8x8()
 display.begin()
 display.clear()
 
